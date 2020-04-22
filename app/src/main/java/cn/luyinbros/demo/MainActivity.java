b162c09d7ba80149a8a8d5fa675f2629ba0f787f
Andy Hong <rainyeveningstreet@gmail.com>
2020-04-22 17:58:34 +0800

    Initial commit

---
+++ app/src/main/java/cn/luyinbros/demo/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.luyinbros.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.luyinbros.android.controller.annotation.Controller;
+import cn.luyinbros.demo.base.BaseActivity;
+
+@Controller(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+
+}
---
+++ app/src/main/java/cn/luyinbros/demo/base/BaseActivity.java
@@ -0,0 +1,39 @@
+package cn.luyinbros.demo.base;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import cn.luyinbros.android.controller.ControllerActivityDelegate;
+import cn.luyinbros.android.controller.ControllerDelegate;
+
+public class BaseActivity extends AppCompatActivity {
+    private final ControllerActivityDelegate mDelegate = ControllerDelegate.create(this);
+
+    @Override
+    protected final void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDelegate.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected final void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mDelegate.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public final void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mDelegate.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDelegate.unbind();
+    }
+}
